ac2bf07534d7e1484e7df34e0ba2006d28839908
Adam Wisniewski <adamw@tbcn.ca>
2013-05-19 17:23:41 -0400

    adding nltk

--- fabfile.py
+++ fabfile.py
@@ -158,7 +158,7 @@ def ipython_notebook_install():
     run('/home/%s/notebookenv/bin/pip install numpy' % env.user)
     run('/home/%s/notebookenv/bin/pip install scipy matplotlib feedparser nose tdaemon' % env.user)
     run('/home/%s/notebookenv/bin/pip install pysqlite PIL markdown requests numexpr cython' % env.user)
-    run('/home/%s/notebookenv/bin/pip install pandas networkx oauth2 beautifulsoup4 tables' % env.user)
+    run('/home/%s/notebookenv/bin/pip install pandas networkx oauth2 beautifulsoup4 tables nltk' % env.user)
 
     # install basemap
     basemap = run('/home/%s/notebookenv/bin/pip freeze |grep basemap' % env.user, warn_only=True)
